8035b46d76f76686d3b5297ad24a4792d8669fda
John Griffiths <gnuchu@gmail.com>
2019-10-01 17:20:11 +0100

    add quiz app

---
+++ questions.php
@@ -0,0 +1,14 @@
+<?php 
+  $questions[0]['question'] = "What was originally considered to be the ninth planet from the Sun but after 1992 had its status as a large planet changed?";
+  $questions[0]['answer'] = "Pluto";
+  
+  $questions[1]['question'] = "How many days are there in February in a leap year?";
+  $questions[1]['answer'] = "29";
+  
+  $questions[2]['question'] = "Give the four initials of the organisation responsible for maintaining a database of drivers in Great Britain?";
+  $questions[2]['answer'] = "DVLA";
+  
+  $questions[3]['question'] = "Which dance move, in which the dancer moves backwards, was made popular by Michael Jackson?";
+  $questions[3]['answer'] = "Moonwalk";
+
+?>
---
+++ quiz.php
@@ -0,0 +1,45 @@
+<?php
+ 
+  require("questions.php");
+  
+  $size_of_question_list = sizeof($questions);
+  
+  // check if we are in the middle of answering a question (if we are, then
+  // the random_number will already be set), otherwise, choose a number
+  
+  if(empty($_REQUEST['random_number'])) {
+    $random_number = rand(0, $size_of_question_list - 1);
+  } else {
+    $random_number = $_REQUEST['random_number'];
+  }
+?>
+ 
+<h1>Quiz</h1>
+ 
+<?php
+ 
+  if(!empty($_REQUEST['user_answer'])) {
+    $user_answer = $_REQUEST['user_answer'];
+    $question_id = $_REQUEST['random_number'];
+    echo "<p>You answered '$user_answer' for Question #$question_id</p>";
+  
+    if($user_answer == $questions[$question_id]['answer']) {
+        echo "Correct :)";
+    } else {
+        echo "Wrong :(";
+    }
+  
+  }
+?>
+ 
+<p><strong>Question: </strong>
+  <?= $questions[$random_number]['question']; ?>
+</p>
+
+<form method='post' action='quiz.php'>
+  <input type='hidden' name='random_number' value='<?= $random_number; ?>' />
+  <input type='text' name='user_answer' />
+  <input type='submit' name='answer_button' value='Answer'/>
+</form>
+ 
+<p><a href='quiz.php'>Another random question</a></p>
