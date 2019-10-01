dd079dd5227ff9dd112e451bc095bb794377874f
John Griffiths <gnuchu@gmail.com>
2019-10-01 19:06:57 +0100

    added new file

--- testsession.php
+++ testsession.php
@@ -6,18 +6,16 @@
   <body>
     <?php
       if(!empty($_REQUEST)) {
-              print_r($_REQUEST);
+        print_r($_REQUEST);
       }
       
       if(!empty($_REQUEST['visitor_name'])) {
-              $_SESSION['visitor_name'] = $_REQUEST['visitor_name'];
+        $_SESSION['visitor_name'] = $_REQUEST['visitor_name'];
       }
       
       if(!empty($_SESSION['visitor_name'])) {
-              echo "<p>Welcome ".htmlentities($_SESSION['visitor_name'])."</p>";
+        echo "<p>Welcome ".htmlentities($_SESSION['visitor_name'])."</p>";
       }
-      
-      
     ?>
     <form method='post' action=''>
       <input type='text' name='visitor_fullname' />
